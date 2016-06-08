fb5b82f51f05db6ed41ce8bbaa8d1be1d5830bb5
Johan Bové <johan.bove@movento.com>
2016-06-08 11:34:12 +0200

    Added initial "product" model to render product list Ticket #5

--- app/pods/styleguide-manual/route.js
+++ app/pods/styleguide-manual/route.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+    model() {
+        return [
+            {
+                title: "Product ",
+                image: "https://upload.wikimedia.org/wikipedia/commons/4/44/Bananas_white_background_DS.jpg",
+                description: "Cavendish bananas are the most common dessert bananas sold.",
+                rating: 4.5,
+                price: 2.49,
+                unit: "kg",
+                currency: "€"
+            }
+        ];
+    }
+
 });

9a9790ce9a8adb3bd55a5990f9c8ab599f8b320e
Johan Bové <johan.bove@movento.com>
2016-06-08 17:11:45 +0200

    Added 404 page Ticket #26

--- app/router.js
+++ app/router.js
@@ -2,11 +2,12 @@ import Ember from 'ember';
 import config from './config/environment';
 
 const Router = Ember.Router.extend({
-  location: config.locationType
+    location: config.locationType
 });
 
 Router.map(function() {
-  this.route('styleguide-manual');
+    this.route('styleguide-manual');
+    this.route('page-404', { path: '/*wildcard' });
 });
 
 export default Router;

541f5a83cb64bc0ff7f665b5a02b51bcde780ad8
Johan Bové <johan.bove@movento.com>
2016-06-15 16:02:37 +0200

    Ticket #30: Ember Sandbox "index" page footer. Changed g-nav-views to allow for dynamic block class names.

--- app/pods/components/g-nav-views/component.js
+++ app/pods/components/g-nav-views/component.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'nav',
-  classNames: ['nav-secondary'],
+  classNames: ['nav-secondary'],      // 'ember-view' will be the first class name
   nav: Ember.inject.service('nav')
 });

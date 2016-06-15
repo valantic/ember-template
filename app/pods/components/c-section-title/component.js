ada031815e2f17144a188bb1c17f67cba36ee3f3
Johan Bové <johan.bove@movento.com>
2016-06-15 14:21:47 +0200

    Ticket #18: Added "bold" title variant. Changed the c-section-title component to use tagName and className.

--- app/pods/components/c-section-title/component.js
+++ app/pods/components/c-section-title/component.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  classNames: ['section-title'],
+  tagName: 'div',
+  classNameBindings: ['isBold:section-title--bold'],  // Adds the class name "section-title--bold" when isBold === true
+  isBold: false                                       // default setting
 });

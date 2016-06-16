0456626a802b8c1ee39bc09929dcd4d7ab9b081b
Johan Bové <johan.bove@movento.com>
2016-06-16 10:28:26 +0200

    Changed nav service according to eslint rules

--- app/pods/nav/service.js
+++ app/pods/nav/service.js
@@ -1,17 +1,15 @@
 import Ember from 'ember';
 
-let links = [{
-  'url': 'views',
-  'label': 'Views',
-  'children': [{
-    'url': 'views.product-page',
-    'label': 'Product Page'
-  }]
-},{
-  'url': 'styleguide-freestyle',
-  'label': 'Living Styleguide'
-}];
-
 export default Ember.Service.extend({
-  links: links
+  links: [{
+    url: 'views',
+    label: 'Views',
+    children: [{
+      url: 'views.product-page',
+      label: 'Product Page'
+    }]
+  }, {
+    url: 'styleguide-freestyle',
+    label: 'Living Styleguide'
+  }]
 });

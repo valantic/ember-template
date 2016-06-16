78529c39c4a9d745bb99feafd9efbb9ad8a6e70c
Johan Bové <johan.bove@movento.com>
2016-06-16 16:01:26 +0200

    Ticket #29: Changed component l-product-page to user service products

--- app/pods/components/g-nav-views/component.js
+++ app/pods/components/g-nav-views/component.js
@@ -3,5 +3,5 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: 'nav',
   classNames: ['nav-secondary'],      // 'ember-view' will be the first class name
-  nav: Ember.inject.service('nav')
+  nav: Ember.inject.service()
 });

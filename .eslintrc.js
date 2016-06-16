5376d49fd76f27600a81f72da58d436423b46638
Johan Bové <johan.bove@movento.com>
2016-06-16 15:52:04 +0200

    Ticket #39: Added project specific options to make eslint work nicely with standard ember js code

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,10 @@ module.exports ={
   "extends": "movento",
   "rules": {
     // Use for project specific settings
+    'import/no-unresolved': 0,
+    'func-names': 0,
+    'space-before-function-paren': 0,
+    'require-jsdoc': 0,
+    'prefer-rest-params': 0
   }
 };

30f7645911b152833ca97d7da9f82e3aca7a900b
Johan Bové <johan.bove@movento.com>
2016-06-16 15:56:46 +0200

    Ticket #30: Fixed footer element

--- app/pods/components/g-footer/component.js
+++ app/pods/components/g-footer/component.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'footer',
+  classNames: ['footer'],
   currentYear: new Date().getFullYear(),
   companyName: 'movento'
 });

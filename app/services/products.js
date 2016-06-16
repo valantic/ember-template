51110b9feacc19381ac1f2cc482dd1407c6e4af5
Johan Bové <johan.bove@movento.com>
2016-06-16 16:00:39 +0200

    Ticket #29: Created services products

---
+++ app/services/products.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export default Ember.Service.extend({
+  items: [{
+    title: 'Bananas',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/4/44/Bananas_white_background_DS.jpg',
+    description: 'Cavendish bananas are the most common dessert bananas sold.',
+    rating: 4.5,
+    price: 2.49,
+    unit: 'kg',
+    currency: '€',
+    isDiscounted: true
+  }, {
+    title: 'Conference Pears',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Pear_DS.jpg/220px-Pear_DS.jpg',
+    description: 'A sweet yellowish- or brownish-green edible fruit which is narrow at the stalk and wider towards the base.',
+    rating: 4.0,
+    price: 2.00,
+    unit: 'kg',
+    currency: '€'
+  }]
+});
---
+++ tests/unit/services/products-test.js
@@ -0,0 +1,13 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('service:products', 'Unit | Service | products', {
+  // Specify the other units that are required for this test.
+  // needs: ['service:foo']
+});
+
+// Replace this with your real tests.
+test('it exists', function(assert) {
+  const service = this.subject();
+
+  assert.ok(service);
+});

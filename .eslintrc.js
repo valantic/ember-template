94b2e91439349c5511d55ef33cd5939d8aa279eb
Johan Bové <johan.bove@movento.com>
2016-06-17 15:46:29 +0200

    Changed eslintrc and added comments to explain the exceptions against the default rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,11 @@ module.exports ={
   "extends": "movento",
   "rules": {
     // Use for project specific settings
-    'import/no-unresolved': 0,
-    'func-names': 0,
-    'space-before-function-paren': 0,
-    'require-jsdoc': 0,
-    'prefer-rest-params': 0
+    'import/no-unresolved': 0,  // For import statements
+    'func-names': 0,            // For router.js
+    'space-before-function-paren': 0, // Prefered to have no space between function and first ()
+    'require-jsdoc': 0,         // For generated code without jsdocs
+    'prefer-rest-params': 0,    // For tests
+    'array-callback-return': 0  // For router.js
   }
 };

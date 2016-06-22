2b60c4724b49eb2a41cec970ced0353a44adf1f0
Johan Bové <johan.bove@movento.com>
2016-06-22 13:00:21 +0200

    Added eslintrc rules for ember

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,9 +4,18 @@ module.exports ={
     // Use for project specific settings
     'import/no-unresolved': 0,  // For import statements
     'func-names': 0,            // For router.js
-    'space-before-function-paren': 0, // Prefered to have no space between function and first ()
-    'require-jsdoc': 0,         // For generated code without jsdocs
-    'prefer-rest-params': 0,    // For tests
-    'array-callback-return': 0  // For router.js
+    'space-before-function-paren': [2, "never"], // Prefered to have no space between function and first ()
+    'require-jsdoc': [1, {      // For generated code without jsdocs
+      'require': {
+        'FunctionDeclaration': true,
+        'MethodDefinition': true,
+        'ClassDeclaration': true
+      }
+    }],
+    'prefer-rest-params': 1,    // For tests
+    'array-callback-return': 1, // For router.js
+    'prefer-const': 1,
+    'newline-after-var': 1,
+    'object-shorthand': 1
   }
 };

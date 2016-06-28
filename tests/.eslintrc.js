ca6e83e266094c6dac0ea4ae2074379d31298d94
Jonas Büchel <jonas.buechel@movento.ch>
2016-06-28 14:55:18 +0200

    Added rules to .eslintrc in test-folder to allow generated tests by ember to pass

--- tests/.eslintrc.js
+++ tests/.eslintrc.js
@@ -1,5 +1,12 @@
 module.exports = {
   env: {
     'embertest': true
+  },
+  rules: {
+    'require-jsdoc': 0,
+    'valid-jsdoc': 0, //Disable jsdoc, because ember generated test does'nt include that
+    'newline-after-var': 0,
+    'prefer-const': 0,
+    'prefer-rest-params': 0
   }
 };

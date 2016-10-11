9925c596ad63a09774495d85c91c93a54b1d47df
Jonas Büchel <jonas.buechel@movento.ch>
2016-10-11 12:12:48 +0200

    Add check for fastboot runner to prevent a failing access to the document object

--- app/mixins/styleguide-functions.js
+++ app/mixins/styleguide-functions.js
@@ -8,24 +8,26 @@ export default Ember.Mixin.create({
    * @param {boolean} styleGuideIsActive true when it is
    */
   toggleStylesheet: function(styleGuideIsActive) {
-    const styleSheetTag = document.querySelector('head > .project-css');
-    const styleSheetStyleguide = '/assets/style-guide.css';
-    // Using the config.modulePrefix makes it possible to make this function a lot simpler
-    // and run consecutive tests.
-    const styleSheetOriginal = `assets/${config.modulePrefix}.css`;
+    if (typeof FastBoot === 'undefined') {
+      const styleSheetTag = document.querySelector('head > .project-css');
+      const styleSheetStyleguide = '/assets/style-guide.css';
+      // Using the config.modulePrefix makes it possible to make this function a lot simpler
+      // and run consecutive tests.
+      const styleSheetOriginal = `assets/${config.modulePrefix}.css`;
 
-    /**
-     * Sets the stylesheet path to styleguide if needed, else restore the original value
-     * @param {string} styleSheetPath Stylesheet path
-     */
-    const setStyleSheet = function(styleSheetPath) {
-      styleSheetTag.setAttribute('href', styleSheetPath);
-    };
+      /**
+       * Sets the stylesheet path to styleguide if needed, else restore the original value
+       * @param {string} styleSheetPath Stylesheet path
+       */
+      const setStyleSheet = function(styleSheetPath) {
+        styleSheetTag.setAttribute('href', styleSheetPath);
+      };
 
-    if (styleGuideIsActive) {
-      setStyleSheet(styleSheetStyleguide);
-    } else {
-      setStyleSheet(styleSheetOriginal);
+      if (styleGuideIsActive) {
+        setStyleSheet(styleSheetStyleguide);
+      } else {
+        setStyleSheet(styleSheetOriginal);
+      }
     }
   }
 });

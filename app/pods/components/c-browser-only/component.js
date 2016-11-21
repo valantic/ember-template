004cb540e3b4b2f2c538e5d0be7a4a11efc2f2ff
Jonas Büchel <jonas.buechel@movento.ch>
2016-11-21 17:02:23 +0100

    Change fastboot detection check, because old version is not supported anymore.

--- app/pods/components/c-browser-only/component.js
+++ app/pods/components/c-browser-only/component.js
@@ -1,12 +1,19 @@
 import Ember from 'ember';
 
-const { Component, inject } = Ember;
+const { Component } = Ember;
 
 // Component is used to stop ember-cli-fastboot rendering content, which is only for a browser.
 // Mostly this prevents to run jQuery, which isn't supported in ember-cli-fastboot
 // Solution by: https://github.com/mitchlloyd/ember-islands/pull/28
 
+// Changed fastboot check according to https://github.com/miguelcobain/ember-paper/pull/441/files
+// Because fastboot.isFastboot is no longer working
+
 export default Component.extend({
   tagName: '',
-  fastboot: inject.service()
+  isFastBoot: Ember.computed(() => {
+    const fastBootIsRunning = !(typeof FastBoot === 'undefined');
+
+    return fastBootIsRunning;
+  })
 });

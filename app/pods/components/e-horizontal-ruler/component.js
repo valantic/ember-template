9b89bc7489fa9066f0dc02a392ad5d4c7e797d4d
Claudio Schäpper <claudio.schaepper@movento.com>
2016-12-14 17:10:48 +0100

    Add e-horizontal-ruler component

---
+++ app/pods/components/e-horizontal-ruler/component.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+// BEGIN-FREESTYLE-USAGE e-horizontal-ruler
+export default Ember.Component.extend({
+  tagName: 'hr',
+  classNames: ['e-horizontal-ruler']
+});
+// END-FREESTYLE-USAGE

81279cac3aeacef9b713bb6b962059e8edefccd2
Claudio Schäpper <claudio.schaepper@movento.com>
2017-01-09 17:04:43 +0100

    describe deprecation-warning in deprecation-workflow-js

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -1,6 +1,6 @@
 window.deprecationWorkflow = window.deprecationWorkflow || {};
 window.deprecationWorkflow.config = {
   workflow: [
-    { handler: 'silence', matchId: 'ember-runtime.enumerable-contains' }
+    { handler: 'silence', matchId: 'ember-runtime.enumerable-contains' } // The Warning comes from the freestyle-collection.js (Ember-freestyle) and is reported here https://github.com/chrislopresto/ember-freestyle/issues/91
   ]
 };

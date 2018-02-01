167a7dd23c882ad9048cf92a45de5b99cd936fb3
Patric Eberle <patric.eberle@cec.valantic.com>
2018-02-01 12:53:50 +0100

    Disables scroll related events to prevent chrome warnings

--- app/app.js
+++ app/app.js
@@ -8,7 +8,17 @@ Ember.MODEL_FACTORY_INJECTIONS = true;
 const App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
-  Resolver
+  Resolver,
+
+  // This fixes a violation issue in Chrome, who expects passive event binding for scroll interrupting events.
+  // see https://developers.google.com/web/updates/2016/06/passive-event-listeners
+  customEvents: { // This will disable the listed event handlers!
+    mousewheel: null,
+    touchstart: null,
+    touchmove: null,
+    touchend: null,
+    touchcancel: null
+  }
 });
 
 loadInitializers(App, config.modulePrefix);
